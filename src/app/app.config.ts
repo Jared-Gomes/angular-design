fe7cbc680abe18627946f32527932fe4645e53ec
Jared Gomes <jdd1489@gmail.com>
2025-05-02 20:29:24 -0400

    Update Material theme and fonts, configure icon registry with rounded style

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  inject,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { MatIconRegistry } from '@angular/material/icon';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: () => {
+        const matIconRegistry = inject(MatIconRegistry);
+        matIconRegistry.setDefaultFontSetClass('material-symbols-rounded');
+        return () => Promise.resolve();
+      },
+    },
+  ],
 };
